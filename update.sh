c0094196b5af5d99fa7eacfe1e0c74ccaab16f3f
Jory Graham <JGraham@walmart.com>
2014-08-26 13:41:39 -0400

    Specify repos as arguments, npm install as well

--- update.sh
+++ update.sh
@@ -1,15 +1,32 @@
 #!/usr/local/bin/zsh
 
-REPOS=(`ls -p | grep '/'`)
+if [[ -n $* ]]; then
+    REPOS=($*)
+else
+    REPOS=(`ls -p | grep '/'`)
+fi
 
 for REPO in $REPOS; do
     cd $REPO
     if [[ -d .git ]]; then
         echo $REPO
+
         git stash
         git checkout master
         git pull
-        echo '------------------------------------------'
+
+        if [[ -f package.json ]]; then
+            npm install
+
+            LOCALLY=`gfind node_modules -maxdepth 1 -lname '*'`
+
+            if [[ -n $LOCALLY ]]; then
+                echo '------------------------------------------'
+                echo $LOCALLY
+            fi
+        fi
+
+        echo '================================================================================'
     fi
     cd -
 done
